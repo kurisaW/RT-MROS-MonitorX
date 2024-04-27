d81d6a10a80f0da1b7e986d0ad0378eba8f3fb57
Yuqiang Wang <yifang.wangyq@foxmail.com>
2024-04-27 19:39:40 +0800

    Organize the repository structure

---
+++ catkin_ws/README.md
@@ -0,0 +1,17 @@
+# catkin_ws
+
+## turtlesim相关指令
+
+```c
+cd catkin_ws
+source install/setup.bash
+ros2 run turtlesim turtlesim_node
+ros2 run turtlesim turtle_teleop_key
+```
+
+## 启动turtlesim发送端节点
+
+```c
+ros2 run my_serial_node  serial_node
+```
+
---
+++ squireline file/SquareLine_Project_events.py
@@ -0,0 +1,104 @@
+
+def passed_test_func(event_struct):
+    return
+
+
+def SearchTopicCallback(event_struct):
+    return
+
+
+def KeyboardCallback(event_struct):
+    return
+
+
+def SubTopicTypeMsgCallback(event_struct):
+    return
+
+
+def SubTopicCallback(event_struct):
+    return
+
+
+def PubTopicCallback(event_struct):
+    return
+
+
+def SearchNodeCallback(event_struct):
+    return
+
+
+def SearchTopicTypeCallback(event_struct):
+    return
+
+
+def SearchTopicNameCallback(event_struct):
+    return
+
+
+def PubTopicTypeCallback(event_struct):
+    return
+
+
+def PubTopicNameCallback(event_struct):
+    return
+
+
+def SubTopicNameMsgCallback(event_struct):
+    return
+
+
+def PubTopicTypeMsgCallback(event_struct):
+    return
+
+
+def PubTopicNameMsgCallback(event_struct):
+    return
+
+
+def TopicListCallback(event_struct):
+    return
+
+
+def NodeListCallback(event_struct):
+    return
+
+
+def key_entry_callback(event_struct):
+    return
+
+
+def topic_select_callback(event_struct):
+    return
+
+
+def node_select_callback(event_struct):
+    return
+
+
+def PubMsgTypeCallback(event_struct):
+    return
+
+
+def PubNodeNameCallback(event_struct):
+    return
+
+
+def SubMsgTypeCallback(event_struct):
+    return
+
+
+def SubNodeNameCallback(event_struct):
+    return
+
+
+def SubTopicNameCallback(event_struct):
+    return
+
+
+def PubNodeIDCallback(event_struct):
+    return
+
+
+def InformationCallback(event_struct):
+    return
+
