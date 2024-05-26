5cb2aaa2b816f80cdb8ba89a4e33c1122bd3f53a
Yuqiang Wang <yifang.wangyq@foxmail.com>
2024-05-26 14:55:47 +0800

    upload soft pack

---
+++ RT-MROS Monitorx/packages/micro_ros_rtthread_component/builder/toolchain.cmake
@@ -0,0 +1,34 @@
+include(CMakeForceCompiler)
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
+
+set(CMAKE_CROSSCOMPILING 1)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(TARGET "D:/toolschain/gcc-arm-none-eabi-10-2020-q4-major-win32/bin/")
+set(CMAKE_C_COMPILER "${TARGET}arm-none-eabi-gcc.exe")
+set(CMAKE_CXX_COMPILER "${TARGET}arm-none-eabi-g++.exe")
+set(CMAKE_AR "${TARGET}arm-none-eabi-ar.exe")
+
+set(CMAKE_C_FLAGS_INIT " -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Dgcc -O0 -gdwarf-2 -g")
+set(CMAKE_CXX_FLAGS_INIT " -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=hard -ffunction-sections -fdata-sections -Dgcc -O0 -gdwarf-2 -g")
+
+
+get_filename_component(script_path "${CMAKE_CURRENT_LIST_FILE}" ABSOLUTE)
+
+message("################################ Toolchain include ######################################")
+string(REGEX MATCH "^(.*hmi-microros)" BSP_ROOT_DIR "${script_path}")
+message("BSP_ROOT_DIR Path: ${BSP_ROOT_DIR}")
+
+include_directories(
+    ${BSP_ROOT_DIR}/rt-thread/include              # kernel include
+    ${BSP_ROOT_DIR}/rt-thread/components/finsh     # finsh include
+    ${BSP_ROOT_DIR}/rt-thread/components/net/sal/include                      # #include <netdb.h>
+    ${BSP_ROOT_DIR}/rt-thread/components/net/sal/include/socket               
+    ${BSP_ROOT_DIR}/rt-thread/components/net/sal/include/socket/sys_socket    # #include <sys/socket.h>
+    ${BSP_ROOT_DIR}/rt-thread/components/net/netdev/include
+    ${BSP_ROOT_DIR}/rt-thread/components/net/netdev/include/arpa              # #include <arpa/inet.h>
+    ${BSP_ROOT_DIR}                                                           # rtconfig.h
+)
+
+set(__BIG_ENDIAN__ 0)
